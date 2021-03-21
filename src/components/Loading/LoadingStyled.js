1b47e219417f8461bb785d28fb209772a46d6750
stepanchaparyan <stepanchaparyan@gmail.com>
2021-03-22 00:06:40 +0400

    Updated Loader

--- src/components/Loading/LoadingStyled.js
+++ src/components/Loading/LoadingStyled.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  height: 100vh;
+  background: lightGray;
+`;
+
 export const LoadingStyled = styled.div`
   position: absolute;
-  top: ${props => `calc(50% + ${props.top}px)`};
+  top: calc(25% - 40px);
   left: calc(50% - 40px);
 `;
