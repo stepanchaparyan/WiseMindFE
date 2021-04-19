3abb5279ba77c1ecf6fa550d94765dd272f25d7c
stepanchaparyan <stepanchaparyan@gmail.com>
2021-04-20 01:21:31 +0400

    change the domain in env file

--- environment.js
+++ environment.js
@@ -1,6 +1,6 @@
 module.exports = {
   // serverUrl: 'http://localhost:5000'
   // serverUrl: 'https://wise-mind-server.herokuapp.com'
-  serverUrl: 'http://54.219.90.126',
+  serverUrl: 'http://www.astutecarepathways.com',
   showLanguageSelector: false
 };
