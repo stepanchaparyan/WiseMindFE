d295b7db348472f9f42d16487cf246343d05cf8a
stepanchaparyan <stepanchaparyan@gmail.com>
2021-04-26 00:24:08 +0400

    changed the domain

--- environment.js
+++ environment.js
@@ -1,6 +1,7 @@
 module.exports = {
   // serverUrl: 'http://localhost:5000'
   // serverUrl: 'https://wise-mind-server.herokuapp.com'
-  serverUrl: 'http://www.astutecarepathways.com',
+  // serverUrl: 'http://www.astutecarepathways.com',
+  serverUrl: 'http://54.151.11.251',
   showLanguageSelector: false
 };
