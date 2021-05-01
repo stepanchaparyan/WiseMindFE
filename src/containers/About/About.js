19d37c1436b3deff7c349201b6229efd93c03f9d
stepanchaparyan <stepanchaparyan@gmail.com>
2021-05-01 23:37:36 +0400

    add test for file downloading

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Module, WelcomeText, LongText, LinkStyled } from './AboutStyled';
-import { LINK } from '../../constants';
+// import { LINK } from '../../constants';
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
@@ -9,7 +10,74 @@ const About = () => {
         <Module>
           <WelcomeText>{'About Us'}</WelcomeText>
           <LongText>{'About Us'}</LongText>
-          <LinkStyled to={LINK.TO.HOME}>{'GO HOME'}</LinkStyled>
+          {/* <LinkStyled to={LINK.TO.HOME}>{'GO HOME'}</LinkStyled> */}
+          <a
+            href="http://api.astutecarepathways.com/media/core_values_1.jpg"
+            download="11111111.png"
+          >
+            <img
+              src="http://api.astutecarepathways.com/media/core_values_1.jpg"
+              alt="W3Schools"
+              width="100"
+              height="100"
+            />
+          </a>
+          <a
+            href="https://api.astutecarepathways.com/media/core_values_1.jpg"
+            download="22222222222.png"
+          >
+            <img
+              src="http://api.astutecarepathways.com/media/core_values_1.jpg"
+              alt="W3Schools"
+              width="100"
+              height="100"
+            />
+          </a>
+          <a href="/media/core_values_1.jpg" download="33333333333.png">
+            <img
+              src="http://api.astutecarepathways.com/media/core_values_1.jpg"
+              alt="W3Schools"
+              width="100"
+              height="100"
+            />
+          </a>
+          <a href="media/core_values_1.jpg" download="444444444.png">
+            <img
+              src="http://api.astutecarepathways.com/media/core_values_1.jpg"
+              alt="W3Schools"
+              width="100"
+              height="100"
+            />
+          </a>
+          <a href="https://www.w3schools.com/images/myw3schoolsimage.jpg" download>
+            <img
+              src="http://api.astutecarepathways.com/media/core_values_1.jpg"
+              alt="W3Schools"
+              width="100"
+              height="100"
+            />
+          </a>
+          <a href="https://www.w3schools.com/images/myw3schoolsimage.jpg" download target="_self">
+            <img
+              src="http://api.astutecarepathways.com/media/core_values_1.jpg"
+              alt="W3Schools"
+              width="100"
+              height="100"
+            />
+          </a>
+          <Link
+            to="http://api.astutecarepathways.com/media/core_values_1.jpg"
+            target="_blank"
+            download
+          >
+            Download
+          </Link>
+          <Link to="/media/core_values_1.jpg" target="_blank" download>
+            Download
+          </Link>
+          <Link to="https://www.w3schools.com/images/myw3schoolsimage.jpg" target="_blank" download>
+            Download
+          </Link>
         </Module>
       </Container>
     </>
