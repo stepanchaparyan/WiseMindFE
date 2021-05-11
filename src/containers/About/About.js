d950e64335d3e76b539f3656b9ee01990198b606
stepanchaparyan <stepanchaparyan@gmail.com>
2021-05-12 02:04:28 +0400

    Added test code for file download

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -11,44 +11,14 @@ const About = () => {
           <WelcomeText>{'About Us'}</WelcomeText>
           <LongText>{'About Us'}</LongText>
           <LinkStyled to={LINK.TO.HOME}>{'GO HOME'}</LinkStyled>
-          {/* <DownLoadLink to="src/assets/test.pdf" target="_blank" download>
+          <a
+            href="https://bucketforfavorites.s3.eu-north-1.amazonaws.com/test.pdf"
+            download="test.pdf"
+            target="_blank"
+            rel="noreferrer"
+          >
             Download
-          </DownLoadLink>
-          <DownLoadLink to="/media/core_values_2.jpg" target="_blank" download="test.jpg">
-            Download ee
-          </DownLoadLink>
-          <a href="/media/core_values_2.jpg" download="w3logo.jpg">
-            XXXXXXXXXXXXX
           </a>
-
-          <a href="/media/core_values_2.jpg" download>
-            XXXXXXXXXXXXX
-          </a>
-
-          <a
-            href="http://api.astutecarepathways.com/media/core_values_1.jpg"
-            download="11111111.jpg"
-          >
-            aaaaaaaa
-          </a>
-
-          <a
-            href="https://api.astutecarepathways.com/media/core_values_1.jpg"
-            download="11111111.jpg"
-          >
-            dddddddd
-          </a>
-
-          <a href="http://api.astutecarepathways.com/media/core_values_1.jpg" download>
-            bbbbbbbbb
-          </a>
-
-          <DownLoadLink to="/media/core_values_2.jpg" target="_blank" download>
-            Download ee
-          </DownLoadLink>
-          <DownLoadLink to="../src/test.jpg" target="_blank" download="test.jpg">
-            Download 1
-          </DownLoadLink> */}
         </Module>
       </Container>
     </>
