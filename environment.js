314470592f3b9b9551576a2b36e765d45c6bb49f
stepanchaparyan <stepanchaparyan@gmail.com>
2021-05-12 23:55:45 +0400

    Updated server url

--- environment.js
+++ environment.js
@@ -1,6 +1,7 @@
 module.exports = {
   // serverUrl: 'http://localhost:5000'
   // serverUrl: 'https://wise-mind-server.herokuapp.com'
-  serverUrl: 'http://api.astutecarepathways.com',
+  // serverUrl: 'http://api.astutecarepathways.com',
+  serverUrl: 'https://apiforastutecarepathways.xyz',
   showLanguageSelector: false
 };
